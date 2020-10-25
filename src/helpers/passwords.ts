4ee4051e0053dfcc37a4311e0f9b2b16f1ace338
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-25 18:37:12 +0100

    feat(helpers): create comparePassword

--- src/helpers/passwords.ts
+++ src/helpers/passwords.ts
@@ -4,3 +4,7 @@ export const encryptPassword = async (password: string) => {
     const salt = await bcryp.genSalt(10)
     return bcryp.hash(password, salt)
 }
+
+export const comparePassword = async (password: string, recivedPassword: string) => {
+    return bcryp.compare(recivedPassword, password)
+}
