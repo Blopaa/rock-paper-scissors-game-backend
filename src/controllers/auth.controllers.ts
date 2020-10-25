14e99b61ec039e81d9aff2856d2afcdc0ac6e49f
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-25 18:37:54 +0100

    feat(controllers, routes): create sign In

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,28 +1,46 @@
-import {Request, Response} from 'express'
-import jwt from 'jsonwebtoken'
-import { encryptPassword } from '../helpers/passwords'
-import User from "../models/User"
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { comparePassword, encryptPassword } from "../helpers/passwords";
+import User from "../models/User";
 
 export const signUp = async (req: Request, res: Response) => {
-    try {
-        const {username, email, password} = req.body
-        console.log(req.body)
-    
-        const user = new User({
-            username,
-            email,
-            password: await encryptPassword(password)
-        })
-    
-        const savedUser = await user.save()
-        
-        const token = jwt.sign({id: savedUser._id}, process.env.SECRET || '', {
-            expiresIn: 172400
-        })
+  try {
+    const { username, email, password } = req.body;
 
-        return res.status(200).json(token)
-        
-    } catch(err) {
-        return res.status(400).json({message: "the body sent is not valid"})
-    }
-}
+    const user = new User({
+      username,
+      email,
+      password: await encryptPassword(password),
+    });
+
+    const savedUser = await user.save();
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.SECRET || "", {
+      expiresIn: 178400,
+    });
+
+    return res.status(200).json(token);
+  } catch (err) {
+    return res.status(400).json({ message: "the body sent is not valid" });
+  }
+};
+
+export const signIn = async (req: Request, res: Response) => {
+  const userFound = await User.findOne({ email: req.body.email });
+
+  if (!userFound) return res.status(404).json({ message: "User not found" });
+
+  const matchPassword = await comparePassword(
+    userFound.password,
+    req.body.password
+  );
+
+  if (!matchPassword)
+    return res.status(400).json({ message: "password mismatch" });
+
+  const token = jwt.sign({ id: userFound._id }, process.env.SECRET || "", {
+    expiresIn: 178400,
+  });
+
+  return res.status(200).json(token);
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,5 +4,6 @@ const router = Router()
 import * as AuthCtrl from '../controllers/auth.controllers'
 
 router.post('/signup', AuthCtrl.signUp)
+router.post('/signin', AuthCtrl.signIn)
 
 export default router
